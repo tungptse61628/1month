e9408634238e493e27319212c66cfe2cf65d82ec
Điền <diendtlse61501@fpt.edu.vn>
2018-05-20 23:48:12 +0700

    chỉnh html

--- Web/Angular/src/app/views/project/new-campaign/new-campaign.component.ts
+++ Web/Angular/src/app/views/project/new-campaign/new-campaign.component.ts
@@ -1,4 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
+import { ProjectService } from '../../../services/project.service';
+import { Cursor, StoreService } from '../../../services/tree.service';
+import { Location } from '@angular/common';
+import * as moment from 'moment';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import {
+  CommentModalComponent,
+  ConfirmModalComponent,
+  ErrorModalComponent,
+  SelectUsersModalComponent,
+  SelectTeamsModalComponent,
+  SelectMembersModalComponent
+} from '../../../cmaComponents/modals';
+import { Router } from '@angular/router';
+import { IMyDpOptions } from 'mydatepicker';
+import { BsModalService } from 'ngx-bootstrap';
+import { Project } from 'app/interfaces/project';
 
 @Component({
   selector: 'app-new-campaign',
@@ -6,10 +27,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-campaign.component.scss']
 })
 export class NewCampaignComponent implements OnInit {
-
+  myDatePickerOptions: IMyDpOptions = {
+    showInputField: true,
+    dateFormat: 'dd/mm/yyyy',
+  };
+  @ViewChild('startDatePicker') startDatePicker;
+  @ViewChild('deadlinePicker') deadlinePicker;
+  newCamForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
   }
 
+  
 }
